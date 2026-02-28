deaab57a8cc029b622ceaf7553a1c8569355a9c3
marpacioglu <62361633+marpacioglu@users.noreply.github.com>
2026-02-28 05:40:51 +0300

    Change import statement for jfs algorithm

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from FS.hibrit_gpt5 import jfs   # change this to switch algorithm 
+from FS.hibrit_gwossa_yeni import jfs   # change this to switch algorithm 
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay,precision_score, recall_score, f1_score
 
 
